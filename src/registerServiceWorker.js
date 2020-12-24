e7f337a43ee4c6e0b13d2bf90bdb5400cbde3ff8
Gustavo Ziger <gustavoziger@gmail.com>
2020-12-23 22:48:49 -0300

    Fixed misspelled word and missing property.

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,14 +1,11 @@
 /* eslint-disable no-console */
 
-import register from 'register-service-worker'
+import { register } from 'register-service-worker'
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready () {
-      console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB'
-      )
+      console.log('App is being served from cache by a service worker.\n')
     },
     registered () {
       console.log('Service worker has been registered.')
--- src/views/Matches.vue
+++ src/views/Matches.vue
@@ -1,24 +1,23 @@
 <template>
   <div>
-
     <v-card class="overline">
       <v-card-title>
         <v-icon x-large>mdi-animation-outline</v-icon><v-divider vertical class="mx-2"></v-divider><span class="text-h5">Matches</span>
       </v-card-title>
       <v-divider></v-divider>
       <v-card-subtitle v-if="matches.length === 0">No matches found for your account.</v-card-subtitle>
-      <v-card-subtitle v-else-if="matces.length === 1">Displaying 1 match.</v-card-subtitle>
+      <v-card-subtitle v-else-if="matches.length === 1">Displaying 1 match.</v-card-subtitle>
       <v-card-subtitle v-else>Displaying {{ matches.length }} matches.</v-card-subtitle>
     </v-card>
 
     <v-card class="overline my-5" v-for="(match, i) in matches" :key="i" cols="12">
       <v-card-title class="text-h6">{{ match.name }}</v-card-title>
-      <v-card-subtitle>Participants: {{ match.participants.length }}<br>Budget:</v-card-subtitle>
+      <v-card-subtitle>Participants: {{ match.participants.length }}<br>Budget: {{ match.budget }}</v-card-subtitle>
       <v-card-actions>
         <v-btn block outlined rounded :to="{ name: 'match', params: { match_id: match.id } }">Go to match.</v-btn>
       </v-card-actions>
     </v-card>
-</div>
+  </div>
 </template>
 
 <script>
